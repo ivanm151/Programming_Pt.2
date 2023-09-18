92fa319af798b1d8cf6e97127f56ba14fac7d5a2
ivanm151 <ivmedv5@gmail.com>
2023-09-18 20:35:18 +0700

    commit2

---
+++ DominoGame.Test/DominoTest.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Domino_Game;
+using System.Windows.Forms;
+using cs_Lab1_Medvedev_8I21;
+
+namespace DominoGame.Test
+{
+    [TestClass]
+    public class DominoTest
+    {
+        Domino domino;
+        Random rand;
+       
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            domino = new Domino();
+        }
+
+        [TestMethod]
+        public void get_value_test()
+        {
+            int expected_range1 = 1;
+            int expected_range2 = 6;
+            int actual1 = domino.get_value()[0];
+            int actual2 = domino.get_value()[1];
+            Assert.IsTrue(expected_range1 <= actual1 && expected_range1 <= actual2 && expected_range2 >= actual1 && expected_range2 >= actual2);
+        }
+        public void set_user_value_test()
+        {
+            rand = new Random();
+            int[] nums = new int[2] { rand.Next(1, 7), rand.Next(1, 7) };
+            domino.set_user_value(nums);
+            Assert.AreEqual(nums, domino.get_value());
+        }      
+    }
+    /*
+    [TestClass]
+    public class ProgramTest
+    {
+        Domino domino;
+        
+        Form1 form1;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            domino = new Domino();
+            form1 = new Form1();
+        }
+        [TestMethod]
+        public void aa()
+        {
+            
+            
+        }
+    }
+    */
+}
+    
---
+++ DominoGame.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DominoGame.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DominoGame.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a6bc060e-ade5-456f-a107-56a82f4322e2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
