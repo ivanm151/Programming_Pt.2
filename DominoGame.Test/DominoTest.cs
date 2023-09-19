8eaa101c0b83e68b2aae4b852029153e12a218c0
ivanm151 <ivmedv5@gmail.com>
2023-09-19 10:40:01 +0700

    commit4

--- DominoGame.Test/DominoTest.cs
+++ DominoGame.Test/DominoTest.cs
@@ -11,12 +11,13 @@ namespace DominoGame.Test
     {
         Domino domino;
         Random rand;
-       
+
 
         [TestInitialize]
         public void TestInitialize()
         {
             domino = new Domino();
+            rand = new Random();
         }
 
         [TestMethod]
@@ -29,34 +30,12 @@ namespace DominoGame.Test
             Assert.IsTrue(expected_range1 <= actual1 && expected_range1 <= actual2 && expected_range2 >= actual1 && expected_range2 >= actual2);
         }
         public void set_user_value_test()
-        {
-            rand = new Random();
+        {          
             int[] nums = new int[2] { rand.Next(1, 7), rand.Next(1, 7) };
             domino.set_user_value(nums);
             Assert.AreEqual(nums, domino.get_value());
-        }      
-    }
-    /*
-    [TestClass]
-    public class ProgramTest
-    {
-        Domino domino;
-        
-        Form1 form1;
-
-        [TestInitialize]
-        public void TestInitialize()
-        {
-            domino = new Domino();
-            form1 = new Form1();
-        }
-        [TestMethod]
-        public void aa()
-        {
-            
-            
         }
     }
-    */
 }
+   
     
