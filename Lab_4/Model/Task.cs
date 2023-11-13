84c3e4d7d66e1325b04e515810b2caa7febbcdae
ivanm151 <ivmedv5@gmail.com>
2023-11-14 00:17:13 +0700

    lab4

---
+++ Lab_4/Model/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4.Model
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public int Params_num { get; set; }
+        public int Vars_num { get; set; }
+        public string Path { get; set; } = ""; 
+    }
+}
--- Lab_4/Model/Test.cs
+++ Lab_4/Model/Test.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Lab_4.Model
 {
-    internal class Test
+   
+    public class Test 
     {
+        public int Count { get; set; }
+        public int Right { get; set; }
+        public int Success { get; set; }
+        public List<Task> Tasks { get; set; }
+        
     }
 }
