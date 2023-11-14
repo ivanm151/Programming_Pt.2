4d13609d0453a70b9a75f5b6e68c0e74b48678fc
ivanm151 <ivmedv5@gmail.com>
2023-11-14 15:57:33 +0700

    lab4 --3

---
+++ Lab_4/Model/ManageVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4.Model
+{
+    public class ManageVM : INotifyPropertyChanged
+    {
+        private Test test;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
