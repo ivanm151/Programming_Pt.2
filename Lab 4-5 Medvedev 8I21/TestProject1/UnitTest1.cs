02711d2df0f140029c8a475f70ed79732a065ff3
ivanm151 <ivmedv5@gmail.com>
2023-12-10 21:17:58 +0700

    Labs 4-5

---
+++ Lab 4-5 Medvedev 8I21/TestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Lab_4_5.model;
+
+namespace TestProject1
+{
+    [TestClass]
+    
+    public class UnitTest1
+    {
+       
+
+        
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
